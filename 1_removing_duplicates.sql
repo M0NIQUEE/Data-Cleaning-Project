a5e96ff78b6d7ba3df57d094bdc62bbdd6414955
m0niquee <moniquenguyen425@gmail.com>
2026-02-02 15:09:14 -0800

    formatting

--- 1_removing_duplicates.sql
+++ 1_removing_duplicates.sql
@@ -1,7 +1,6 @@
 -- We are going to select from layoff_staging and look for duplicates
 --      and display them so we can see which companies are dupes of one another to be able to delete
-SELECT
-    company,
+SELECT company,
     location,
     industry,
     total_laid_off,
@@ -11,8 +10,7 @@ SELECT
     funds_raised_millions,
     COUNT(*) AS duplicate_count
 FROM layoffs_staging
-GROUP BY
-    company,
+GROUP BY company,
     location,
     industry,
     total_laid_off,
@@ -24,25 +22,22 @@ HAVING COUNT(*) > 1;
 
 -- Confirming that we do indeed have duplicates in our data, our next goal is to delete the duplicates
 --   but making sure we are still keeping one row and not deleting both
-
 DELETE FROM layoffs_staging
 WHERE ctid NOT IN (
-    SELECT MIN(ctid)
-    FROM layoffs_staging
-    GROUP BY
-        company,
-    location,
-    industry,
-    total_laid_off,
-    date,
-    stage,
-    country,
-    funds_raised_millions
-);
-
+        SELECT MIN(ctid)
+        FROM layoffs_staging
+        GROUP BY company,
+            location,
+            industry,
+            total_laid_off,
+            date,
+            stage,
+            country,
+            funds_raised_millions
+    );
+    
 -- After success it should be able to return no data, meaning we have deleted
 --      all the found duplicates.
-
 -- Rows went from 2361 to 2356
 SELECT *
 FROM layoffs_staging;
