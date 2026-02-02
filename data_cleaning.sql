ed5e593d6f70407553f45f1854d41fa57910391c
m0niquee <moniquenguyen425@gmail.com>
2026-02-02 13:50:09 -0800

    update

---
+++ data_cleaning.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM layoffs;
+
+-- 1. Remove duplicates
+-- 2. Standardize the Data
+-- 3. Null Values or blank values
+-- 4. Remove Any columns
+
+
+CREATE TABLE layoffs_staging AS
+SELECT *
+FROM layoffs;
+
+SELECT *
+FROM layoffs_staging;
