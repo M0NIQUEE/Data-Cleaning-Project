91629497087eb18f3491502f835013fcc45e047c
m0niquee <moniquenguyen425@gmail.com>
2026-02-02 15:09:42 -0800

    Formatting

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,16 +1,13 @@
 SELECT *
 FROM layoffs;
+CREATE TABLE layoffs_staging AS
+SELECT *
+FROM layoffs;
+SELECT *
+FROM layoffs_staging;
 
 -- What we are trying to achieve in this project for data cleaning
 -- 1. Remove duplicates
 -- 2. Standardize the Data
 -- 3. Null Values or blank values
 -- 4. Remove Any columns
-
-
-CREATE TABLE layoffs_staging AS
-SELECT *
-FROM layoffs;
-
-SELECT *
-FROM layoffs_staging;
